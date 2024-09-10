b6bef3343a163fa0a14a63ead9ddd9a94a696ebd
Kenneth Edoho <kennethtibele@gmail.com>
2024-09-11 00:19:28 +0100

    Setting up Application structure

---
+++ Application/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> CreateBlogAsync(T blog);
+        Task UpdateBlogAsync(T blog);
+        Task DeleteBlogAsync(T blog);
+    }
+}
